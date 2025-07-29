d58322ebf4fdc0007a963558935016f755f1dd7a
LogicSekai <cameliayu112@gmail.com>
2025-07-29 17:50:32 +0700

    init taildwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/ui']
+  devtools: { enabled: false },
+  modules: ['@nuxt/image', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
